5a10d9e729249ccfd1c0b4a81be81cf53e48465d
Caroline LS <carolinelds@gmail.com>
2022-04-04 06:57:06 -0300

    Preenche tela 'historico'

--- src/components/TelaHistorico.js
+++ src/components/TelaHistorico.js
@@ -7,7 +7,8 @@ export default function TelaHistorico() {
         <>
             <Topo />
             <Div>
-                <p>TELA HISTORICO</p>
+                <h1>Histórico</h1>
+                <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
             </Div>
             <Menu />
         </>
@@ -15,5 +16,23 @@ export default function TelaHistorico() {
 }
 
 const Div = styled.div`
+    box-sizing: border-box;
     margin-top: 70px;
+    font-family: 'Lexend Deca';
+    padding: 28px 20px 0px 20px;
+
+    h1 {
+        font-weight: 400;
+        font-size: 23px;
+        line-height: 29px;
+        color: #126BA5;
+        margin-bottom: 17px;
+    }
+
+    p {
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 22px;
+        color: #666666;
+    }
 `;
